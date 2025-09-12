984104847524c75b100a9dfa98ef6cf5a8a4a921
wailadivyansh <76157070+wailadivyansh@users.noreply.github.com>
2025-09-12 20:22:50 +0530

    Create day1_.c

---
+++ day1_2.c
@@ -0,0 +1,20 @@
+//Taking Input From the user to Add Two numbers
+
+#include <stdio.h>
+
+int main(){
+
+int a,b ;
+printf("enter a");
+scanf("%d" , &a) ;
+
+printf("enter b");
+scanf("%d" , &b);
+
+
+
+printf("%d" , a+b);
+
+
+return 0;
+}
