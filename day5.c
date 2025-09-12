b0f04d463ec2d72b3ce7c1cb2b6b6770bfc7d03b
wailadivyansh <76157070+wailadivyansh@users.noreply.github.com>
2025-09-12 20:45:27 +0530

    Add files via upload

---
+++ day5.c
@@ -0,0 +1,13 @@
+//Write a program to convert temperature from Celsius to Fahrenheit.
+
+#include <stdio.h>
+int main(){
+    float temp ;
+ printf("enter the temprature in celcius");
+scanf("%f" ,&temp);
+
+printf("temprature in ferhenite %f" , (temp*9/5)+32);
+return 0;
+
+
+}
