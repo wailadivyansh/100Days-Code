d6d53f0f2b853fbfcd82af3b9057119217001992
wailadivyansh <76157070+wailadivyansh@users.noreply.github.com>
2025-09-12 19:58:48 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,32 @@
+/*Write a program to input two numbers and display their sum.
+Input 1: 
+3 4
+Input 2:
+-1 20
+*/
+
+#include <stdio.h>
+
+int main(){
+
+int a=4;
+int b=3;
+printf("A is 4\n");
+printf("B is 3\n");
+printf("The sum of TWO No. a and b is :- %d \n" ,a+b);
+
+int c=-1;
+int d=20;
+printf("C is -1\n");
+printf("D is 20\n");
+printf("The sum of TWO No. a and b is :- %d\n" ,c+d);
+
+
+
+
+
+
+
+
+return 0;
+}
