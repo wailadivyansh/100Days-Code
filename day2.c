77cd888089d1a3f8287f7ea7a00905b83adb473b
wailadivyansh <76157070+wailadivyansh@users.noreply.github.com>
2025-09-12 20:26:06 +0530

    Add files via upload

---
+++ day2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+int a,b ;
+printf("enter a");
+scanf("%d" , &a) ;
+
+printf("enter b");
+scanf("%d" , &b);
+
+
+
+printf("%d\n" , a+b);
+printf("%d\n" , a-b);
+printf("%d\n" , a*b);
+printf("%d\n" , a/b);
+
+
+
+
+
+
+return 0;
+}
