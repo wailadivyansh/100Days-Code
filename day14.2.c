80b890e7b38d758f88449dc1eaaac7f87dbec05f
wailadivyansh <76157070+wailadivyansh@users.noreply.github.com>
2025-09-29 12:56:03 +0530

    Update day14.2.c

--- day14.2.c
+++ day14.2.c
@@ -1,5 +1,15 @@
 //Write a program to print the product of even numbers from 1 to n.
 #include <stdio.h>
 int main() {
-int
-} 
+int a,c;
+long long b=1;
+printf("Enter a  No. :\n");
+scanf("%d",&a);
+for(c=2;c<a;c=c+2){
+printf("%d\t",c);
+b=b*c;
+}
+printf("\n");
+printf("product of even no. from 1 to %d is %lld",a,b);
+return 0;
+}
